86b7af1fbc91f96d774f1db56cc59c11e4331dae
DavidEmelyanov <91591340+DavidEmelyanov@users.noreply.github.com>
2022-05-04 23:27:17 +0700

    Добавлен проект логики NoteApp.Model

--- NoteApp.Model/Properties/AssemblyInfo.cs
+++ NoteApp.Model/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("NoteAppUI")]
+[assembly: AssemblyTitle("NoteApp.Model")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NoteAppUI")]
+[assembly: AssemblyProduct("NoteApp.Model")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("32d1694c-b061-4ac0-802e-c5c555e959b1")]
+[assembly: Guid("aebfc6a8-413b-4c22-ad47-e0729f927d37")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
--- NoteAppUI/Program.cs
+++ NoteAppUI/Program.cs
@@ -6,17 +6,17 @@ using System.Windows.Forms;
 
 namespace NoteAppUI
 {
-    internal static class Program
-    {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
-    }
+	static class Program
+	{
+		/// <summary>
+		/// Главная точка входа для приложения.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+		}
+	}
 }
